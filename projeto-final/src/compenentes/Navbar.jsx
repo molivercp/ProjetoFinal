c25ade19bf5e2fa49d26718d9b83563ce4e28f96
Geração Tech <geracaotech@digitalcollege.internal>
2025-05-02 10:40:28 -0300

    Adicionei bootstrap

--- projeto-final/src/compenentes/Navbar.jsx
+++ projeto-final/src/compenentes/Navbar.jsx
@@ -1,7 +1,8 @@
 function Navbar () {
     return (
-        <div>
-        <h1>Olá, eu sou mateus e estou usando o React</h1></div>
+        <div className="bg-primary border border-primary" >
+        <h1 className="bg-danger display-1">Olá, eu sou mateus e estou usando o React</h1>
+        </div>
     )
 
 }
